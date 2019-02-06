a10d1b41a55fdd4805dcd4db4fe30f11e18a0eec
Yusuf Bahadur <yusuf.bahadur@gmail.com>
2019-02-06 11:06:56 -0800

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println(multiply(3,4));
+    }
+
+    public static int multiply(int a, int b) {
+        int c = a * b;
+        return c;
+    }
+}
